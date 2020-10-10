cbe85acef4b37ceaacfda8036a7ed095028fa9eb
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2020-10-09 23:24:36 -0700

    Add generate_name in new Serialize class

--- src/yaml_serialize.py
+++ src/yaml_serialize.py
@@ -1,22 +1,38 @@
 import yaml
 
 
-class Object:
-    def __init__(self, object):
-        self.object = object
+class ObjectDict:
+    """Generate dictionary representation of object arguments"""
+    def __init__(self, _object: object):
+        self._object = _object
 
     def get_attrs(self):
-        return self.object.__dict__
+        return self._object.__dict__
 
 
 class YamlDump:
-    def __init__(self, serialize_object, outfile):
-        self.serialize_object = serialize_object
+    def __init__(self, object_dict: dict, outfile: str):
+        self.object_dict = object_dict
         self.outfile = outfile
 
     def generate_yaml(self):
-        return yaml.dump(self.serialize_object)
+        """Generate yaml string from object_dict"""
+        return yaml.dump(self.object_dict)
 
     def write_file(self):
         with open(self.outfile, "w") as file:
             return file.write(self.generate_yaml())
+
+
+class Serialize:
+    def __init__(self, object_to_serialize, name: str = False):
+        self.object_to_serialize = object_to_serialize
+        self.object_dict = ObjectDict(object_to_serialize)
+        self.default_name = name
+        self.name = self.generate_name()
+
+    def generate_name(self):
+        if self.default_name is False:
+            return self.object_to_serialize.__class__.__name__
+        else:
+            return self.default_name
--- tests/test_yaml_serialize.py
+++ tests/test_yaml_serialize.py
@@ -6,31 +6,31 @@ class MyClass:
         self.name = "myclass"
 
 
-class TestObject:
+class TestObjectDict:
     def setup_method(self):
         myclass = MyClass()
-        self.ser = yaml_serialize.Object(myclass)
+        self.object_dict = yaml_serialize.ObjectDict(myclass)
 
-    def test_serialize_exist(self):
+    def test_object_dict_exist(self):
         myclass = MyClass()
-        yaml_serialize.Object(myclass)
+        yaml_serialize.ObjectDict(myclass)
 
-    def test_serialize_attrs(self):
-        assert hasattr(self.ser, "object")
+    def test_object_dict_attrs(self):
+        assert hasattr(self.object_dict, "_object")
 
     def test_get_attrs(self):
-        assert isinstance(self.ser.get_attrs(), dict)
-        assert self.ser.get_attrs().get("name") == "myclass"
+        assert isinstance(self.object_dict.get_attrs(), dict)
+        assert self.object_dict.get_attrs().get("name") == "myclass"
 
 
 class TestYamlDump:
     def setup_method(self):
         myclass = MyClass()
-        serialize_object = yaml_serialize.Object(myclass).get_attrs()
-        self.yaml_dump = yaml_serialize.YamlDump(serialize_object, "file")
+        object_dict = yaml_serialize.ObjectDict(myclass).get_attrs()
+        self.yaml_dump = yaml_serialize.YamlDump(object_dict, "file")
 
     def test_yaml_dump_attrs(self):
-        assert hasattr(self.yaml_dump, "serialize_object")
+        assert hasattr(self.yaml_dump, "object_dict")
         assert hasattr(self.yaml_dump, "outfile")
 
     def test_generate_yaml(self):
@@ -42,3 +42,17 @@ class TestYamlDump:
     def test_write_file(self):
         write = self.yaml_dump.write_file()
         assert isinstance(write, int)
+
+
+class TestSerialize:
+    def setup_method(self):
+        self.myclass = MyClass()
+
+    def test_generate_name(self):
+        ser = yaml_serialize.Serialize(self.myclass)
+        assert ser.generate_name() == "MyClass"
+        assert ser.name == "MyClass"
+
+        ser = yaml_serialize.Serialize(self.myclass, "MyName")
+        assert ser.generate_name() == "MyName"
+        assert ser.name == "MyName"
